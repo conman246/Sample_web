6eda76c99937c80faf1289bcaebc5094a44cdebc
connor McCrone <your.email@example.com>
2013-04-08 12:52:42 -0400

    Added bootstrap and applied layout to the page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleWeb::Application.config.secret_token = '7f834e0faa285a2dc070161e81c8d44cbf22e7f9b7d95d47cabe5cd9ef86cbb8497a0ba8dc54f117411b5bc2e8637449d2960362d2ef48ba505ca2d5dff1bb1e'
